1960f06ca1870aead44caf22e41796b2089d507a
light2001 <light2001@sina.com>
2019-05-22 18:44:22 +0800

    测试重定向

--- Src/src/main/java/com/example/demo/web/HomeController.java
+++ Src/src/main/java/com/example/demo/web/HomeController.java
@@ -2,11 +2,20 @@ package com.example.demo.web;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 
 @RestController
 public class HomeController {
     @RequestMapping("Index")
     public String Index(){
         return "你好，世界22";
+    }
+
+    /*测试Baidu重定向*/
+    @RequestMapping("Baidu")
+    public ModelAndView Index1(){
+        ModelAndView mv = new ModelAndView();
+        mv.setViewName("redirect:http://www.baidu.com");
+        return mv;
     }
 }
