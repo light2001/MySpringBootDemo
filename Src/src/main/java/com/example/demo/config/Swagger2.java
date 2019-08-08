682e9876cea6f2e4efcbfa7953473f1b44c9d3ea
light2001 <light2001@sina.com>
2019-08-08 13:51:42 +0800

    增加jpa，调整过时写法

--- Src/src/main/java/com/example/demo/config/Swagger2.java
+++ Src/src/main/java/com/example/demo/config/Swagger2.java
@@ -6,6 +6,7 @@ import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
@@ -25,12 +26,13 @@ public class Swagger2 {
     }
 
     private ApiInfo apiInfo() {
+
         return new ApiInfoBuilder()
                 //自定义信息可按需求填写
                 .title("Spring Boot中使用Swagger构建RESTful APIs")
-                .description("测试")
+                .description("SpringBootDemo")
                 .termsOfServiceUrl("http://www.baidu.com")
-                .contact("阿华")
+                .contact(new Contact("阿华","",""))
                 .version("1.0")
                 .build();
     }
